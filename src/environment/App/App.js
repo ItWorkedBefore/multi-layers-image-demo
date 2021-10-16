50d511a6caa83505c625ffe674ee434646dbf535
Shinriel <the.shinriel@gmail.com>
2021-10-16 17:13:50 +0200

    Add details about the repository on the page (#2)

--- src/environment/App/App.js
+++ src/environment/App/App.js
@@ -4,11 +4,27 @@ import {PngRandomAvatar, SvgRandomAvatar, ConfigWithSeed} from 'lib';
 import classes from './App.module.css';
 
 const App = () => (
-  <div className={classes.container}>
-    <PngRandomAvatar />
-    <SvgRandomAvatar />
-    <ConfigWithSeed />
-  </div>
+  <>
+    <div className={classes.description}>
+      <p>
+        This page show you how works the library{' '}
+        <a href="https://www.npmjs.com/package/multi-layers-image">
+          multi-layers-image
+        </a>
+      </p>
+      <p>
+        You can show the code of the page on the demo project{' '}
+        <a href="https://github.com/ItWorkedBefore/multi-layers-image-demo">
+          ItWorkedBefore/multi-layers-image-demo
+        </a>
+      </p>
+    </div>
+    <div className={classes.container}>
+      <PngRandomAvatar />
+      <SvgRandomAvatar />
+      <ConfigWithSeed />
+    </div>
+  </>
 );
 
 export default App;
